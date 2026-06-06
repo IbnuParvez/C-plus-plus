b14319df9afface2f2402912eb5f4d37c968d02b
IbnuParvez <parvezfatma98@gmail.com>
2026-06-06 10:45:13 +0300

    Add loop example programs

---
+++ class-example/do-while.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int dice = 1;
+        cout << "Enter the value you prefer in the dice: \n";
+        cin >> dice;
+        do{
+            cout << ;
+        } while (dice <= 6);
+    return 0;
+}
---
+++ class-example/for-loop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int sum = 0;
+    for(int i = 0;i<= 100;i++){
+        sum +=i;
+        cout << "total sum is: " << sum << "\n";
+    }
+    return 0;
+}
