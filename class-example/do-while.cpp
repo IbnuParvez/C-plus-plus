43ac84b0b4415241add1cdc9e308dbf83ee793d9
IbnuParvez <parvezfatma98@gmail.com>
2026-06-06 10:52:16 +0300

    Validate dice input in do-while example
    Initialize dice to 0 and add isValid flag. Prompt the user until
    they enter a value between 1 and 6, printing success or error
    messages. Fix syntax and formatting issues.

--- class-example/do-while.cpp
+++ class-example/do-while.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 
 using namespace std;
-int main(){
-    int dice = 1;
-        cout << "Enter the value you prefer in the dice: \n";
+
+int main() {
+    int dice = 0;
+    bool isValid = false;
+
+    do {
+        cout << "Enter a dice value (1-6): ";
         cin >> dice;
-        do{
-            cout << ;
-        } while (dice <= 6);
-    return 0;
+        if (dice >= 1 && dice <= 6) {
+            cout << "Success! You entered a valid dice value: " << dice << endl;
+            isValid = true;
+        } else {
+            cout << "Invalid input. A dice must be between 1 and 6. Try again.\n\n";
+        }
+    } while (!isValid);
 }
