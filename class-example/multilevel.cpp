af445da80fa4eb6f787bbb207c68096cf5d9be3e
IbnuParvez <parvezfatma98@gmail.com>
2026-07-11 09:38:22 +0300

    multilevel inheritance is demonstrated

---
+++ class-example/multilevel.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Family{
+    public:
+        void gene(){
+            cout << "our family is funny\n";
+        }
+};
+
+class Father: public Family{};
+class Child: public Father{};
+
+int main(){
+    Child myGene;
+    myGene.gene();
+    return 0;
+}
